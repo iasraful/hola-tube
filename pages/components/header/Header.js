13a1094742c490eff51cb6baebebac2d8808c300
Asraful Islam <iasraful321@gmail.com>
2021-07-23 18:49:09 +0600

    problem solve

--- pages/components/header/Header.js
+++ pages/components/header/Header.js
@@ -1,11 +1,23 @@
 import Image from "next/dist/client/image";
-import {HomeIcon}from "@heroicons/react/outline"
+import {BadgeCheckIcon,
+
+    CollectionIcon,
+    HomeIcon,
+LightningBoltIcon,
+SearchIcon,
+UserIcon}from "@heroicons/react/outline"
 import logo from '../../../public/pngfind.com-hola-png-5873084.png'
 import HeaderItems from "./HeaderItems";
 export default function Header() {
     return (
         <header className="">
             <HeaderItems title="HOME" Icon={HomeIcon}/>
+            <HeaderItems title="Trending" Icon={LightningBoltIcon}/>
+            <HeaderItems title="Verefied" Icon={BadgeCheckIcon}/>
+            <HeaderItems title="Collections" Icon={CollectionIcon}/>
+            <HeaderItems title="Search" Icon={SearchIcon}/>
+            <HeaderItems title="Account" Icon={UserIcon}/>
+            
             <Image
             className="object-contain"
             alt="nav image"
--- pages/components/header/HeaderItems.js
+++ pages/components/header/HeaderItems.js
@@ -1,7 +1,7 @@
 function HeaderItems({Icon, title}) {
     return (
         <div className="flex flex-col items-center cursor-pointer w-12 group sm:w-20 hover:text-white">
-            <Icon className="h-8 mb-1 mt-3 hover:animate-bounce"/>
+            <Icon className="h-8 mb-1 mt-3 group-hover:animate-bounce"/>
             <p className= "tracking-widest opacity-100 group-hover:opacity-100">{title}</p>
         </div>
     )
